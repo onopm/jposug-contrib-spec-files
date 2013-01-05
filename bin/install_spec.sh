2ccfb9c7b8099a651228911e674eacf9a6a481c3
TAKI,Yasushi <taki@justplayer.com>
2013-01-05 16:56:55 +0900

    InstallSpec: 複数のファイルのインストールが間違っていたのを修正
    複数のファイルのインストールが間違っていたのを修正した。
    その他、GNUmakefileの事情で引数に.protoを設定していたが、
    .specファイルを設定しても動くようにした。

--- bin/install_spec.sh
+++ bin/install_spec.sh
@@ -1,18 +1,39 @@
 #!/bin/sh
 
 if [ -z $1 ];then
-    echo "install_spec.sh protofile"
+    echo "install_spec.sh .proto"
     exit 1
 fi
 
-SPECNAME=${1%.proto}.spec
-INFONAME=${1%.proto}.info
+if [ -z $PKGBUILD_IPS_REPO ];then
+    PKGBUILD_IPS_REPO=localhost
+fi
+
+if [ ${1%.proto} != $1 ];then
+    NODENAME=${1%.proto}
+elif [ ${1%.spec} != $1 ];then
+    NODENAME=${1%.spec}
+else
+    NODENAME=$1
+fi
+
+SPECNAME=${NODENAME}.spec
+INFONAME=${NODENAME}.info
 S2I=./spec2ipsname.list
 
-PKGNAME=`cat ${S2I} | awk -F: '{if($1 == "'${SPECNAME}'"){print $2}}'`
-echo "[${PKGNAME}]"
+PKGNAME=`cat ${S2I} | awk -F: '
+{
+    if($1 == "'${SPECNAME}'") {
+        num=split($2,arr," ")
+      	for(i = 1; i <= num; i++) {
+            printf "pkg://'${PKGBUILD_IPS_REPO}'/%s ",arr[i]
+	}
+    }
+}
+'`
+
+echo "Install packages : ${PKGNAME}"
 if [ ! -z $PKGNAME ];then
-    echo sudo pkg install -v ${PKGNAME}
     sudo pkg install -v ${PKGNAME}
     RES=$?
     if [ $${RES} = 0 -o $${RES} = 4 ] ; then

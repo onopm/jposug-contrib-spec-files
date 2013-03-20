8f208c97d2d3f6d0f09ba4272034d64d1d0a364c
YAMAMOTO Takashi <yamachan@selfnavi.com>
2013-03-20 22:10:13 +0900

    余計なファイルをコミットしてしまいました。戻します。

--- bin/install_spec.sh
+++ bin/install_spec.sh
@@ -34,11 +34,11 @@ PKGNAME=`cat ${S2I} | awk -F: '
 
 echo "Install packages : ${PKGNAME}"
 if [ ! -z $PKGNAME ];then
-    ${SUPERCMD} pkg install -v ${PKGNAME}
+    sudo pkg install -v ${PKGNAME}
     RES=$?
     if [ ${RES} = 0 -o ${RES} = 4 ] ; then
 	echo Get info files ${INFONAME}
-	${SUPERCMD} pkg info ${PKGNAME} > ${INFONAME}
+	sudo pkg info ${PKGNAME} > ${INFONAME}
     fi
     exit $RES
 else
